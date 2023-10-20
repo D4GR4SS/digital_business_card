b3088fb2fd36fe260bb2a90b6925156c329bd48c
Davide Grassitelli <dagrassitelli@gmail.com>
2023-10-20 13:47:24 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/digital_business_card/',
   plugins: [react()],
+  base: '/digital_business_card/',
 });
